fa13106f057704112a04ced834ade0e8cf50e44b
Wolfram Schmidt <wolfram.schmidt@phamos.eu>
2024-06-26 12:12:44 +0200

    Revert "feature/33-adding-doctype-implementation-with-field-notes-to-app"

--- phamos/phamos/doctype/implementation/implementation.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, phamos.eu and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class Implementation(Document):
-	pass
