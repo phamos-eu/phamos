485effb3e3a8fbe5cf4cc5f3ce6a288c54e780f4
Sayali <sayali.deshmukh@phamos.eu>
2025-03-05 14:43:46 +0530

    phamos/phamos#146

--- phamos/phamos/doctype/implementation/implementation.js
+++ phamos/phamos/doctype/implementation/implementation.js
@@ -2,11 +2,14 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Implementation", {
-	after_save:function(frm){
+	setup:function(frm){
+		console.log('**')
+	}
+	/*after_save:function(frm){
 		add_row_to_child_table(frm)
-	},
+	},*/
 });
-
+/*
 function add_row_to_child_table(frm) {
     let row = frm.add_child('status_information'); // 'items' is the child table fieldname
     row.maturity_level = frm.doc.maturity_level;
@@ -15,4 +18,4 @@ function add_row_to_child_table(frm) {
     row.status = frm.doc.status;
     row.time = frappe.datetime.now_datetime();
     frm.refresh_field('status_information'); // Refresh child table UI
-}
+}*/
--- phamos/phamos/doctype/implementation/implementation.py
+++ phamos/phamos/doctype/implementation/implementation.py
@@ -6,17 +6,18 @@ from frappe.model.document import Document
 
 
 class Implementation(Document):
-	def before_save(self):
-		self.remove_duplicate_rows_of_status_history()
+	pass
+	# def before_save(self):
+	# 	self.remove_duplicate_rows_of_status_history()
 
-	def remove_duplicate_rows_of_status_history(self):
-		unique_items = set()
-		new_items = []
+	# def remove_duplicate_rows_of_status_history(self):
+	# 	unique_items = set()
+	# 	new_items = []
 
-		for row in self.status_information:  # Replace 'items' with your child table fieldname
-			if row.time not in unique_items:
-				unique_items.add(row.time)
-				new_items.append(row)
+	# 	for row in self.status_information:  # Replace 'items' with your child table fieldname
+	# 		if row.time not in unique_items:
+	# 			unique_items.add(row.time)
+	# 			new_items.append(row)
 
-		self.status_information = new_items
+	# 	self.status_information = new_items
 		#self.save(ignore_permissions=True)
