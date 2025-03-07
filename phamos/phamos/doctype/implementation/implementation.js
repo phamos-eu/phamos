48bde0137ba5bbe6205dc1daffeac128121fae06
Sayali <sayali.deshmukh@phamos.eu>
2025-03-07 15:09:05 +0530

    phamos/phamos#146

--- phamos/phamos/doctype/implementation/implementation.js
+++ phamos/phamos/doctype/implementation/implementation.js
@@ -1,14 +1,23 @@
 // Copyright (c) 2025, phamos.eu and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("Implementation", {
+/*frappe.ui.form.on("Implementation", {
 	setup:function(frm){
 		console.log('**')
-	}
+		if(!frm.is_new()){
+			frappe.call({
+				method: "phamos.phamos.doctype.implementation.implementation.get_financial_history",
+				args: {'name':frm.doc.name},
+				callback: function (r) {
+					console.log('************')
+				},
+			});
+		}
+	}*/
 	/*after_save:function(frm){
 		add_row_to_child_table(frm)
 	},*/
-});
+/*});*/
 /*
 function add_row_to_child_table(frm) {
     let row = frm.add_child('status_information'); // 'items' is the child table fieldname
