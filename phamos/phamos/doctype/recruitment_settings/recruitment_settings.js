63f771959bfd4d50475b7a242e5ef7b17dba89d5
beingeek <furqan.790@outlook.com>
2025-03-17 08:22:11 +0000

    refactor: remove unused interview slot handling code

--- phamos/phamos/doctype/recruitment_settings/recruitment_settings.js
+++ phamos/phamos/doctype/recruitment_settings/recruitment_settings.js
@@ -6,22 +6,3 @@
 
 // 	},
 // });
-
-frappe.ui.form.on("Interview Booking Slots", {
-	interview_slot(frm, ctd, cdn) {
-		var d = locals[ctd][cdn];
-		if (d.interview_slot) {
-			frappe.call({
-				method: "frappe.client.get",
-				args: {
-					doctype: "Interview Booking Slot",
-					filters: { name: d.criteria_name },
-				},
-				callback: function (data) {
-					frappe.model.set_value(ctd, cdn, "from_time", data.message.from_time);
-					frappe.model.set_value(ctd, cdn, "to_time", data.message.to_time);
-				},
-			});
-		}
-	},
-});
