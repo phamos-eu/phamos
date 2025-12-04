854098e5bbb0d4a74882f7b91dbd8111a483371e
kalsoom357 <kalsoomhussain357@gmail.com>
2025-12-04 17:20:23 +0500

    Team Doctype updated

--- phamos/phamos/doctype/team/team.py
+++ phamos/phamos/doctype/team/team.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2025, phamos.eu and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import nowdate
 
 
 class Team(Document):
 	pass
+
+	def create_team_capacity_ledger_entry(doc, method):
+		frappe.get_doc({
+			"doctype": "Team Capacity Ledger",
+			"team": doc.team_name,
+			"total_team_capacity": doc.total_team_capacity,
+			"date": nowdate()
+		}).insert(ignore_permissions=True)
