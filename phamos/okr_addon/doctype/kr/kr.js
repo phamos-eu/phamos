b43be88f51e1c5cb8c58f351716f1062b0e4d684
Kamal Singh <kamal.singh@phamos.eu>
2026-02-26 15:24:31 +0530

    phamos/phamos#589: ability to create child OKRs directly from Parent OKR

--- phamos/okr_addon/doctype/kr/kr.js
+++ phamos/okr_addon/doctype/kr/kr.js
@@ -1,8 +1,13 @@
 // Copyright (c) 2026, phamos.eu and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("KR", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("KR", {
+	refresh(frm) {
+		// Create OKR linked to this KR (Parent KR) - user can set Parent OKR on the new form
+		if (frm.doc.name) {
+			frm.add_custom_button(__('Create OKR'), function() {
+				frappe.new_doc('OKR', { parent_kra: frm.doc.name }).run();
+			}, __('Create'));
+		}
+	},
+});
