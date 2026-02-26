6f0054d562964d3cb388eb7d27d7f32e2c9daa46
Furqan Asghar <furqan.79000@gmail.com>
2026-02-26 18:25:09 +0500

    Enhance interview scheduling: validate interview round and fetch configuration from Recruitment Settings

--- phamos/public/js/job_applicant.js
+++ phamos/public/js/job_applicant.js
@@ -2,16 +2,40 @@ frappe.ui.form.on("Job Applicant", {
 	refresh: function (frm) {
 		if (!frm.doc.status == "") {
 			frm.add_custom_button(__("Send Interview Slot booking link"), () => {
-				frappe.call({
-					method: "invite_for_interview",
-					doc: frm.doc,
-					callback(r){
-						frappe.msgprint("Invite sent");
-						if (r.message && r.message.custom_shortlisted == "Yes") { 
-							frm.reload_doc();
+				// Prompt for Interview Round selection (filtered by Designation)
+				frappe.prompt(
+					{
+						label: __("Interview Round"),
+						fieldname: "interview_round",
+						fieldtype: "Link",
+						options: "Interview Round",
+						reqd: 1,
+						get_query: function() {
+							return {
+								filters: {
+									designation: frm.doc.designation || ""
+								}
+							};
 						}
-					}
-				})
+					},
+					function(values) {
+						frappe.call({
+							method: "invite_for_interview",
+							doc: frm.doc,
+							args: {
+								interview_round: values.interview_round
+							},
+							callback(r){
+								frappe.msgprint("Invite sent");
+								if (r.message && r.message.custom_shortlisted == "Yes") { 
+									frm.reload_doc();
+								}
+							}
+						});
+					},
+					__("Select Interview Round"),
+					__("Send")
+				);
 			});
 		}
 	}
