8b73135fa0d14d400b49a92ce6f0448e187c5baa
Roque Vera <roque.vera@phamos.eu>
2026-03-12 15:06:34 -0300

    feat: add Timesheet Summary report with filters and data retrieval

---
+++ phamos/phamos/report/timesheet_summary/timesheet_summary.js
@@ -0,0 +1,25 @@
+// Copyright (c) 2026, phamos.eu and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Timesheet Summary"] = {
+    "filters": [
+        {
+            "fieldname": "time_period",
+            "label": __("Time Period"),
+            "fieldtype": "Select",
+            "options": [
+                "Last Week",
+                "Last 2 Weeks",
+                "Last Month",
+                "Last 3 Months",
+                "Last 6 Months"
+            ],
+            "default": "Last Month",
+            "reqd": 1, // Makes the filter mandatory
+            "on_change": function() {
+                // Automatically refreshes the report when the selection changes
+                frappe.query_report.refresh();
+            }
+        }
+    ]
+};
