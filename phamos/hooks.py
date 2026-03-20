9a9c516c569df18108983d195efbd8466cf15681
Simon Wanyama <simon.wanyama@phamos.eu>
2026-03-20 18:11:54 +0000

    fix: fix frappe list bulk edit

--- phamos/hooks.py
+++ phamos/hooks.py
@@ -19,7 +19,8 @@ app_include_css = [
 # app_include_js = "/assets/phamos/js/phamos.js"
 app_include_js = [
     "phamos.bundle.js",
-    "https://code.highcharts.com/highcharts.js"
+    "https://code.highcharts.com/highcharts.js",
+    "/assets/phamos/js/frappe_list_bulk_edit_override.js",
 ]
 
 
