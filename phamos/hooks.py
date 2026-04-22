936f214e6cf8fa96f8813ee4aa3094d1b1a47f60
sonali <sonali.narkar@phamos.eu>
2026-04-22 09:45:08 +0200

    fix:correct hook function

--- phamos/hooks.py
+++ phamos/hooks.py
@@ -196,7 +196,7 @@ scheduler_events = {
     "daily": [
         "phamos.api.send_daily_timesheet_comment_summary",
         "phamos.phamos.doctype.team.team.update_all_teams_weekly_holidays",
-        "phamos.phamos.doctype.monthly_implementation_summery.create_monthly_implementation_summaries.create_monthly_implementation_summaries",
+        "phamos.phamos.doctype.monthly_implementation_summary.create_monthly_implementation_summaries.create_monthly_implementation_summaries",
         "phamos.api.send_daily_birthday_wishes"
     ],
     "monthly": [
