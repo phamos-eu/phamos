8f3e57c255512d3b00dce7e5443945493ab1027b
Kamal Singh <kamal.singh@phamos.eu>
2026-05-06 16:21:38 +0530

    revert dev changes from release

--- phamos/hooks.py
+++ phamos/hooks.py
@@ -159,12 +159,6 @@ doc_events = {
     "Delivery Note": {
         "on_submit": "phamos.phamos.doctype.monthly_implementation_summary.monthly_implementation_summary.update_mis_timesheets_on_delivery_note_submit",
     },
-    "Implementation": {
-        "after_insert": "phamos.gitlab_integration.gitlab_group_utils.create_group_and_project_if_not_exists"
-    },
-    "Customer": {
-        "on_update": "phamos.gitlab_integration.gitlab_group_utils.update_gitlab_avatar_on_customer"
-    },
     "Interview Feedback": {
         "on_update": "phamos.phamos.hr.interview_summary.trigger_interview_summary",
         "on_submit": "phamos.phamos.hr.interview_summary.trigger_interview_summary",
