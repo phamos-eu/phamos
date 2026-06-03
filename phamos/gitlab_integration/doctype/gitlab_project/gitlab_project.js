3e5cf680ec3b4e070931eed3d65d98459204cd10
Kalsoom Akhtar <kalsoom.akhtar@phamos.eu>
2026-06-03 10:29:33 +0500

    sync issues project wise

--- phamos/gitlab_integration/doctype/gitlab_project/gitlab_project.js
+++ phamos/gitlab_integration/doctype/gitlab_project/gitlab_project.js
@@ -1,8 +1,18 @@
 // Copyright (c) 2025, phamos.eu and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("GitLab Project", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("GitLab Project", {
+    refresh(frm) {
+        frm.add_custom_button(__("Sync Issues"), () => {
+            frappe.call({
+                method: "phamos.gitlab_integration.gitlab_utils.sync_issues_for_project",
+                args: {
+                    project_name: frm.doc.name,
+                },
+                callback: (r) => {
+                    frappe.msgprint(r.message || __("Issues synced!"));
+                },
+            });
+        });
+    },
+});
