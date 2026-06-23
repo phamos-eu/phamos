b34a6444f08210f2e8c590b65f35434652f41e02
realespee <wanyamsp@gmail.com>
2026-06-23 20:09:28 +0300

    feat: connect email group and newsletter to marketing content,
    - enable creating newsletters from marketing content
    - track marketing event subscribers in Email Group
    - code the naming for Newsletter to include year and month
    - add connection for marketing content in Newsletter

--- phamos/phamos/doctype/marketing_content/marketing_content.js
+++ phamos/phamos/doctype/marketing_content/marketing_content.js
@@ -1,2 +1,26 @@
-// // // Copyright (c) 2026, phamos.eu and contributors
-// // // For license information, please see license.txt
+frappe.ui.form.on('Marketing Content', {
+	refresh(frm) {
+		if (!frm.is_new()) {
+			frm.add_custom_button(__('Create Newsletter'), function () {
+				if (!frm.doc.email_group) {
+					frappe.msgprint({
+						title: __('Email Group Required'),
+						message: __('Please set an Email Group on this record before creating a Newsletter.'),
+						indicator: 'orange'
+					});
+					return;
+				}
+
+				frappe.call({
+					method: 'phamos.phamos.doctype.marketing_content.marketing_content.create_newsletter_for_event',
+					args: { event_name: frm.doc.name },
+					callback(r) {
+						if (r.message) {
+							frappe.set_route('Form', 'Newsletter', r.message);
+						}
+					}
+				});
+			});
+		}
+	}
+});
