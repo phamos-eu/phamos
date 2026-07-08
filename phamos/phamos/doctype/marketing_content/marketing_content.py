b41ae6f0dbc4cc30015c3083e46ab5f41adc9b24
Kalsoom Akhtar <kalsoom.akhtar@phamos.eu>
2026-07-08 12:55:39 +0500

    Making selectable customer on pahmos setting

--- phamos/phamos/doctype/marketing_content/marketing_content.py
+++ phamos/phamos/doctype/marketing_content/marketing_content.py
@@ -231,7 +231,9 @@ def create_ticket_order(event_name, attendees, qty, invoice_data=None):
 
     # --- Find or create customer from primary attendee ---
     primary = attendees[0]
-    customer = _resolve_customer(primary["full_name"], primary["email"], is_company_order, invoice_data)
+    customer = frappe.db.get_single_value("phamos Settings", "walk_in_customer")
+    if needs_tax_invoice:
+        customer = _resolve_customer(primary["full_name"], primary["email"], is_company_order, invoice_data)
 
     # --- Item rate ---
     item_price = frappe.db.get_value(
@@ -269,7 +271,16 @@ def create_ticket_order(event_name, attendees, qty, invoice_data=None):
         "delivery_date": frappe.utils.today(),
     })
 
-    item_tax_template = frappe.db.get_value("Marketing Content", event_name, "item_tax_template")
+    for att in attendees:
+        so.append("items", {
+            "item_code": item_code,
+            "qty": 1,
+            "rate": rate,
+            "additional_notes": att.get("full_name") + "\n" + att.get("email"),
+            "item_tax_template": item_tax_template,
+            "delivery_date": frappe.utils.today(),
+        })
+
     if item_tax_template:
         tax_detail_rows = frappe.get_all(
             "Item Tax Template Detail",
