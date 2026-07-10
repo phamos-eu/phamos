35e29e2c480618dd14c6db8dcf35d9ef152bd3b6
Fariz Khanzada <fariz.khanzada@phamos.eu>
2026-07-10 15:17:58 +0500

    feat: move Mailcow DAV password generation from User to Email Account

--- phamos/public/js/mailcow_email_account.js
+++ phamos/public/js/mailcow_email_account.js
@@ -1,29 +1,30 @@
-frappe.ui.form.on("User", {
+frappe.ui.form.on("Email Account", {
 	refresh: function (frm) {
 		frm.trigger("set_custom_buttons");
 	},
 
 	set_custom_buttons: function (frm) {
-		// frm.reload_doc();
 		if (!frm.is_new()) {
 			frm.add_custom_button(__('Generate DAV Password'), () => {
+				const username = frm.doc.email_id;
+
+				if (!username) {
+					frappe.msgprint(__('Email ID is required to generate DAV password.'));
+					return;
+				}
+
 				frappe.call({
 					method: "phamos.mailcow_integration.dav_password.generate_dav_password",
 					args: {
-                        username: frm.doc.name,
-                    },
-					callback(r){
-						if (r.message && r.message.status == "ok") { 
+						username: username,
+					},
+					callback(r) {
+						if (r.message && r.message.status == "ok") {
 							frappe.msgprint("DAV Password Generated Successfully!");
 						}
-					}
-				})
-				// frm.reload_doc();
-
+					},
+				});
 			}, __("Mailcow"));
-
 		}
 	},
-
-	
 });
