409dc8fb018528ee41a8b594b730db38e30b30d5
Kalsoom Akhtar <kalsoom.akhtar@phamos.eu>
2026-08-27 11:26:04 +0500

    Make fields readonly

--- phamos/phamos/doctype/implementation_chapter_revision/implementation_chapter_revision.py
+++ phamos/phamos/doctype/implementation_chapter_revision/implementation_chapter_revision.py
@@ -6,9 +6,5 @@ from frappe.model.document import Document
 
 
 class ImplementationChapterRevision(Document):
-	def validate(self):
-		if not self.is_new():
-			frappe.throw(frappe._("Implementation Chapter Revisions are immutable and cannot be edited."))
-
 	def on_trash(self):
 		frappe.throw(frappe._("Implementation Chapter Revisions are immutable and cannot be deleted."))
